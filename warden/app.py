b1e53501ead71a1187f5771b92e6070515db71e6
Ethan Haynes <eghaynes17@gmail.com>
2019-02-17 16:57:14 -0600

    breaking up logic for udp tcp

--- warden/app.py
+++ warden/app.py
@@ -9,7 +9,7 @@ sock_errors = (
     socket.timeout
 )
 
-def check_connection( host, port, udp, ipv6 ):
+def check_connection( host, port, udp, ipv6, timeout, message ):
     s_settings = {
         'sock_type'  : socket.IPPROTO_UDP if udp else socket.IPPROTO_TCP,
         'addr_family': None,
@@ -43,31 +43,73 @@ def check_connection( host, port, udp, ipv6 ):
         proto=s_settings['sock_type']
     )
 
+    sock.settimeout(timeout)
+    result = None
     try:
-        result = sock.connect_ex( ( host, port ) )
+        if udp:
+            sock.sendto(bytes(message, 'utf-8'), ( host, port ))
+            result = sock.recv(1024)
+            print(result)
+        else: result = sock.connect_ex( ( host, port ) )
     except sock_errors as e:
-        return print(e)
+        print(e)
+    finally:
+        sock.close()
 
-    sock.close()
     print({
         'host': host,
         'port': port,
-        'up'  : ( result == 0 ),
+        'up'  : ( result != None ),
         'ip'  : s_settings['ip']
     })
 
-@click.command()
+@click.group()
+@click.pass_context
 @click.option('-h','--host', 'host', type=str,
               required=True, help='host string -- [HOSTNAME|IP]')
 @click.option('-p', '--port', type=int, required=True,
               help='port to connect to -- [PORT]')
-@click.option('--udp', is_flag=True,
-              help='enable UDP for protocol -- default is TCP')
 @click.option('-6', '--ipv6', is_flag=True,
               help='enable IPv6 support for addressing -- default is IPv4')
-def app( host, port, udp, ipv6 ):
-    """Simple program that checks a single port on given host."""
-    check_connection( host, port, udp, ipv6 )
+@click.option('-t', '--timeout', type=int, default=5,
+              help='specify timeout for port scan -- default is 5 seconds')
+def app( ctx, host, port, ipv6, timeout ):
+    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
+    # by means other than the `if` block below
+    ctx.ensure_object(dict)
+
+    ctx.obj['host'] = host
+    ctx.obj['port'] = port
+    ctx.obj['ipv6'] = ipv6
+    ctx.obj['timeout'] = timeout
+    pass
+
+
+@app.command()
+@click.pass_context
+def tcp( ctx ):
+    check_connection(
+        ctx.obj['host'],
+        ctx.obj['port'],
+        False,
+        ctx.obj['ipv6'],
+        ctx.obj['timeout'],
+        None
+    )
+
+@app.command()
+@click.pass_context
+@click.option('-uM', '--udp-message', 'udp_message', type=str, required=True,
+              help='port to connect to -- [PORT]')
+def udp( ctx, udp_message ):
+    check_connection(
+        ctx.obj['host'],
+        ctx.obj['port'],
+        True,
+        ctx.obj['ipv6'],
+        ctx.obj['timeout'],
+        udp_message
+    )
 
 if __name__ == '__main__':
-    app()
+    cli(obj={})
